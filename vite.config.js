a1939df1d49527da4fdb0a1d5dae237a9487b353
NeoCai-CN <cncaijianing@gmail.com>
2025-08-07 17:00:50 +0800

    部署 GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 ﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-    base: '/my-company-site/',   // ⭐ 必须与你的 GitHub 仓库名完全一致
-    plugins: [vue()],
+export default defineConfig(({ mode }) => {
+    return {
+        base: mode === 'production' ? '/my-company-site/' : '/',
+        plugins: [vue()],
+    }
 })
