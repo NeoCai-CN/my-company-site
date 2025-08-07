449b843eb220d732bd6e38d05d0982fbedb57774
NeoCai-CN <cncaijianing@gmail.com>
2025-08-07 16:41:02 +0800

    添加 docs 文件夹以部署 GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    base: '/my-company-site/',   // ⭐ 必须与你的 GitHub 仓库名完全一致
+    plugins: [vue()],
 })
